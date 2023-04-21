0bd92c5c1dc43d5b17ce9dcf746c569429955565
Emile <emile.schockaert@student.kuleuven.be>
2023-04-21 15:18:40 +0200

    Initialized working website

--- src/Controller/BookBinderController.php
+++ src/Controller/BookBinderController.php
@@ -2,7 +2,24 @@
 
 namespace App\Controller;
 
-class BookBinderController
-{
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+class BookBinderController extends AbstractController
+{
+    private array $stylesheets;
+
+    public function __construct()
+    {
+        $this->stylesheets[] = 'main.css';
+    }
+
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('main.html.twig', [
+            'stylesheets'=> $this->stylesheets
+        ]);
+    }
 }
