d489741e4ffe836fdc327ce553177b1211c928a9
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-04-28 13:57:44 +0200

    basic file setup

---
+++ src/Controller/BookBinderController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BookBinderController extends AbstractController
+{
+    private array $stylesheets;
+
+    public function __construct()
+    {
+        $this->stylesheets[] = 'main.css';
+    }
+
+    #[Route('/login', name: 'login')]
+    public function login(): Response
+    {
+        return $this->render('login.html.twig', [
+            'stylesheets' => $this->stylesheets
+        ]);
+    }
+}
