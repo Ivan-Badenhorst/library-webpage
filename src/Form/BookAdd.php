25daced3c3c785f5e08db51b6e682a69d5281317
aymer <aymericmaixentphonsavanh.baume@student.kuleuven.be>
2023-05-25 15:04:43 +0200

    added forms plus js file

---
+++ src/Form/BookAdd.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class BookAdd extends AbstractType
+{
+    /**
+     * Form used for search functionality on the home-page
+     *
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('add_to_favorites', SubmitType::class);
+    }
+}
