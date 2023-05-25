e56e9546ae7f1d73971b34be318b7d4a8260c1e7
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-05-25 09:35:22 +0200

    annotations and comments added for search.js

--- public/js/search.js
+++ public/js/search.js
@@ -1,23 +1,42 @@
-//
+/**
+ * @fileoverview JavaScript file for XYZ home-page:  to update and display books
+ * @version 1.0.0
+ */
+
+/**
+ * @author Ivan Badenhorst
+ * @since 2023-05-25.
+ */
 
 
-    var searchBar = document.getElementById('book-search-form');
-    var genreFilter = document.getElementById('filter_genre');
-    var checkboxes = genreFilter.getElementsByTagName("input");
-    var books;
 
-(function() {
+    const searchBar = document.getElementById('book-search-form'); //search bar on home page
+    const genreFilter = document.getElementById('filter_genre');   //genres selector on home page
+    const checkboxes = genreFilter.getElementsByTagName("input"); //list of checkboxes
+    let books; //array used for storing books that are displayed
+
+    /**
+     * event listener for search bar submit event
+     * @listens {Event} submit
+     * @description extracts the value entered in the search bar and calls fetchBooks() on this value
+     */
     searchBar.addEventListener('submit', function(event) {
         event.preventDefault();
-        var titleInput = document.getElementById('book_search_search_term');
-        var title = titleInput.value;
+        let titleInput = document.getElementById('book_search_search_term');
+        let title = titleInput.value;
         fetchBooks(title);
     });
 
-    genreFilter.addEventListener('change', function (event){
 
-        var titleInput = document.getElementById('book_search_search_term');
-        var title = "";
+    /**
+     * event listener for the list of genres
+     * @listens {Event} change
+     * @description refreshes the displayed books based on the newly selected list of genres
+     */
+    genreFilter.addEventListener('change', function (){
+
+        let titleInput = document.getElementById('book_search_search_term');
+        let title = "";
         title += titleInput.value;
         if( title === "" || title == null){
             title = "%"
@@ -29,38 +48,65 @@
 
 
 
-
+    /**
+     * Gets a list of all selected Genres in the checkboxes given on the home-page
+     *
+     * @returns {Array.<String>} Array of strings describing the selected genres
+     */
     function getGenres(){
-        //get genres selected
-        var selectedValues = [];
+        //array to store selected genres
+        let genres = [];
 
-        for (var i = 0; i < checkboxes.length; i++) {
+        //loop over the checkboxes and include those that have been selected
+        for (let i = 0; i < checkboxes.length; i++) {
             if (checkboxes[i].type === "checkbox" && checkboxes[i].checked) {
-                selectedValues.push(checkboxes[i].value.toUpperCase());
+                genres.push(checkboxes[i].value.toUpperCase());
             }
         }
-        return selectedValues;
+        return genres;
     }
 
+
+    /**
+     * Brief description of the function.
+     *
+     * @param {String} title - a search term for looking for books -> function searches by title
+     * @returns {void}: function call displayBooks() as final  + updates global parameter books with a list of books
+     * as return on the search
+     */
     function fetchBooks(title) {
-        var xhr = new XMLHttpRequest();
+
+        let xhr = new XMLHttpRequest();
+        //open URL that returns JSON search results
         xhr.open('GET', '/search/' + title, true);
         xhr.onload = function() {
             if (xhr.status === 200) {
                 books = JSON.parse(xhr.responseText);
+                //display books on page
                 displayBooks(getGenres());
             }
         };
         xhr.send();
     }
 
+
+
+    /**
+     * Displays the books in the global parameter books based on given genres
+     *
+     * @param {Array.<String>} genres - List of genres to be included in the display
+     * @return {void}
+     * @description no return - function generes html code and adds to a display for each book in the global
+     * variable books, but only if this book had genres within the list of genres provided.
+     */
     function displayBooks(genres) {
-        var booksContainer = document.getElementById('books');
+        let booksContainer = document.getElementById('books');
         booksContainer.innerHTML = ''; // Clear existing content
 
         books.forEach(function(book) {
+            //only display book if it contains at least one of the selected genres
             if(checkStringForElements(book.genres.toUpperCase(), genres)) {
-                var text = '<div id="books-container">' +
+                let text = '<div id="books-container">' +
                     '<div class="book">' +
                     '<img loading="lazy" src="' + book.book_cover + '" alt="Entity Image">' +
                     '<div>' +
@@ -68,7 +114,6 @@
                     '<p> By ' + book.author + '</p>' +
                     '<p>Genre: ' + book.genres + '</p>'
                     + '</div>'
-                    // +'</div>'+
                     + '</div>';
 
                 booksContainer.innerHTML += text;
@@ -79,9 +124,13 @@
     }
 
 
-
+    /**
+     * Checks if any elements in a list can be found within a given String
+     *
+     * @param {String} string - A String within which you want to check if elements of the list are valid
+     * @param {Array.<String>} elements - A list of strings
+     * @returns {Boolean} indicates whether any element in the array is present in string
+     */
     function checkStringForElements(string, elements) {
         return elements.some(element => string.includes(element));
     }
-
-})();
