f2bbbe90b29fd17e0a2c24b86814ed9e0a233bf4
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-05-25 10:36:36 +0200

    annotations and comments added for BookSearch.php

--- src/Form/BookSearch.php
+++ src/Form/BookSearch.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @fileoverview Form for search functionality
+ * @version 1.0
+ */
+
+/**
+ * @author Ivan Badenhorst
+ * @since 2023-04-20.
+ */
 
 namespace App\Form;
 
@@ -9,6 +18,12 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class BookSearch extends AbstractType
 {
+    /**
+     * Form used for search functionality on the home-page
+     *
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
