dc3e6db0ea62e923c916265a40d85e3edb999078
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-05-28 19:33:17 +0200

    reading list testing panther complete

--- tests/Panther/BookInfoTest.php
+++ tests/Panther/BookInfoTest.php
@@ -9,6 +9,54 @@ use Symfony\Component\Panther\PantherTestCase;
 class BookInfoTest extends PantherTestCase
 {
 
+    public function testFavourites(): void{
+
+        //when not logged in button should not be there:
+
+        //open without login:
+        $client = static::createPantherClient(); // Your app is automatically started using the built-in web server
+        $crawler = $client->request('GET', '/logout');
+        $crawler = $client->request('GET', '/book-info/26');
+
+        // Find the button with ID "book_add_add_to_favorites"
+        $buttonExists = $crawler->filter('#book_add_add_to_favorites')->count() > 0;
+
+        // Assert the existence of the button
+        $this->assertFalse($buttonExists);
+
+        //when logged in the button should be there:
+
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->filter('form[name="login"]')->form();
+        $form['login[email]'] = 'newUser@email.com'; // Update with the desired email
+        $form['login[password]'] = 'password'; // Update with the desired password
+
+        $crawler = $client->submit($form);
+
+        $crawler = $client->request('GET', '/book-info/26');
+
+        // Find the button with ID "book_add_add_to_favorites"
+        $buttonExists = $crawler->filter('#book_add_add_to_favorites')->count() > 0;
+
+        // Assert the existence of the button
+        $this->assertTrue($buttonExists);
+
+
+        $button = $crawler->filter('#book_add_add_to_favorites')->first();
+        // Get the text of the button
+        $buttonTextBeforeClick = $button->text();
+        echo "before: ".$buttonTextBeforeClick;
+        // Click on the button
+        $button->click();
+        sleep(1);
+
+        // Get the text of the button again
+        $buttonTextAfterClick = $button->text();
+        echo "after: ".$buttonTextAfterClick;
+
+        self::assertTrue((($buttonTextAfterClick=="Add to favorites" && $buttonTextBeforeClick == "Remove from favorites")||($buttonTextBeforeClick=="Add to favorites" && $buttonTextAfterClick == "Remove from favorites")));
+    }
+
     public function testOpen(): void{
         //open without login:
         $client = static::createPantherClient(); // Your app is automatically started using the built-in web server
@@ -53,52 +101,7 @@ class BookInfoTest extends PantherTestCase
 
     }
 
-    public function testFavourites(): void{
 
-        //when not logged in button should not be there:
-
-        //open without login:
-        $client = static::createPantherClient(); // Your app is automatically started using the built-in web server
-        $crawler = $client->request('GET', '/book-info/26');
-
-        // Find the button with ID "book_add_add_to_favorites"
-        $buttonExists = $crawler->filter('#book_add_add_to_favorites')->count() > 0;
-
-        // Assert the existence of the button
-        $this->assertFalse($buttonExists);
-
-        //when logged in the button should be there:
-
-        $crawler = $client->request('GET', '/login');
-        $form = $crawler->filter('form[name="login"]')->form();
-        $form['login[email]'] = 'newUser@email.com'; // Update with the desired email
-        $form['login[password]'] = 'password'; // Update with the desired password
-
-        $crawler = $client->submit($form);
-
-        $crawler = $client->request('GET', '/book-info/26');
-
-        // Find the button with ID "book_add_add_to_favorites"
-        $buttonExists = $crawler->filter('#book_add_add_to_favorites')->count() > 0;
-
-        // Assert the existence of the button
-        $this->assertTrue($buttonExists);
-
-
-        $button = $crawler->filter('#book_add_add_to_favorites')->first();
-        // Get the text of the button
-        $buttonTextBeforeClick = $button->text();
-        echo "before: ".$buttonTextBeforeClick;
-        // Click on the button
-        $button->click();
-        sleep(1);
-
-        // Get the text of the button again
-        $buttonTextAfterClick = $button->text();
-        echo "after: ".$buttonTextAfterClick;
-
-        self::assertTrue((($buttonTextAfterClick=="Add to favorites" && $buttonTextBeforeClick == "Remove from favorites")||($buttonTextBeforeClick=="Add to favorites" && $buttonTextAfterClick == "Remove from favorites")));
-    }
 
 
 
