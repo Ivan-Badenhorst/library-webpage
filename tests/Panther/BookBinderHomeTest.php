a9b36475d43bc8ddf8ba485a67fee77b7b6fe28e
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-05-28 14:20:36 +0200

    panther test for home page -> check search

--- tests/Panther/BookBinderHomeTest.php
+++ tests/Panther/BookBinderHomeTest.php
@@ -11,7 +11,6 @@ class BookBinderHomeTest extends PantherTestCase
 
         $client = static::createPantherClient(); // Your app is automatically started using the built-in web server
         $crawler = $client->request('GET', '/');
-
         //testing navigator:
         $ulElement = $crawler->filter('header ul')->first();
         $this->assertNotEmpty($ulElement, 'The <ul> element in the header was not found.');
@@ -29,6 +28,120 @@ class BookBinderHomeTest extends PantherTestCase
             // Add more assertions as needed
         }
 
+
+        $logoutButton = $crawler->filter('a.normalButton')->first();
+        $this->assertNotEmpty($logoutButton, 'The <a> element was not found.');
+
+        $linkText = $logoutButton->text();
+        $expectedText = 'Login'; // Update this value with the expected link text
+
+        $this->assertSame($expectedText, $linkText, 'The login button is not rendered correctly');
+       // $client->close();
+    }
+
+    public function testOpenWithLogin(): void{
+        $client = static::createPantherClient(); // Your app is automatically started using the built-in web server
+        $crawler = $client->request('GET', '/');
+
+        $logoutButton = $crawler->filter('a.normalButton')->first();
+        $crawler = $client->click($logoutButton->link());//$logoutButton->click();
+
+        // Get the current URL after redirection
+        $currentUrl = $client->getCurrentURL();
+
+        // Assert the expected redirection URL
+        $expectedRedirectUrl = 'http://127.0.0.1:9080/login'; // Update with the expected URL
+        $this->assertSame($expectedRedirectUrl, $currentUrl, 'Login button did not redirect to correct page');
+
+        //next I login:
+        $form = $crawler->filter('form[name="login"]')->form();
+
+        $formExists = $crawler->filter('form[name="login"]')->count() > 0;
+        $this->assertTrue($formExists, 'The login form does not exist on the page.');
+
+        $form['login[email]'] = 'newUser@email.com'; // Update with the desired email
+        $form['login[password]'] = 'password'; // Update with the desired password
+
+        // Submit the form
+        $crawler = $client->submit($form);
+        $currentUrl = $client->getCurrentURL();
+
+        //did we redirect to the home page
+        $expectedRedirectUrl = 'http://127.0.0.1:9080/';
+        $this->assertSame($expectedRedirectUrl, $currentUrl, 'URL after login clicked');
+
+        //check if login button is now logout
+        $logoutButton = $crawler->filter('a.normalButton')->first();
+        $this->assertNotEmpty($logoutButton, 'The <a> element was not found.');
+
+        $linkText = $logoutButton->text();
+        $expectedText = 'Logout'; // Update this value with the expected link text
+
+        $this->assertSame($expectedText, $linkText, 'The logout button is not rendered correctly');
+
+        //$client->close();
+    }
+
+    public function testBookSearch(): void{
+
+        $client = static::createPantherClient(); // Your app is automatically started using the built-in web server
+        $crawler = $client->request('GET', '/');
+
+        // Find the input element by its ID and fill in the text
+        $searchInput = $crawler->filterXPath('//input[@id="book_search_search_term"]')->first();
+        $searchInput->sendKeys('hunger');
+
+        // Find the submit button by its ID and click it
+        $submitButton = $crawler->filterXPath('//button[@id="book_search_search"]')->first();
+        $crawler = $client->submit($submitButton->form());
+
+        //next I will check the search results: -> for hunger I know there are 2 results
+        // Find the container div with the class "book-listings"
+        $containerDiv = $crawler->filter('div.book-listings')->first();
+
+
+        $this->waitForSearch($crawler, $client, 2);
+        $crawler = $client->refreshCrawler();
+        $divCount = $containerDiv->filter('div')->count();
+
+        // Assert the expected number of div elements
+        $expectedCount = 2; // Update with the expected count
+        $this->assertEquals($expectedCount*2, $divCount, 'The actual count of book elements does not match the expected count.');
+
+
+    }
+
+    /**
+     * @param \Symfony\Component\Panther\DomCrawler\Crawler|\Symfony\Component\DomCrawler\Crawler $crawler
+     * @param \Symfony\Component\Panther\Client $client
+     * @param $expectedResults
+     */
+    public function waitForSearch(\Symfony\Component\Panther\DomCrawler\Crawler|\Symfony\Component\DomCrawler\Crawler $crawler, \Symfony\Component\Panther\Client $client, $expectedResults): void
+    {
+        $timeout = 10; // Maximum time to wait in seconds
+        $start = time();
+        while (time() - $start < $timeout) {
+            $innerDivs = $crawler->filter('.book-listings .book-container');
+
+            if ($innerDivs->count() < 40 && $innerDivs->count() >= $expectedResults) {
+                // Desired condition met, exit the loop
+                break;
+            }
+
+            sleep(1); // Wait for 1 second before rechecking the condition
+            $crawler = $client->refreshCrawler(); // Refresh the crawler to get updated content
+        }
+    }
+
+    private function login($client, $crawler){
+        $logoutButton = $crawler->filter('a.normalButton')->first();
+        $crawler = $client->click($logoutButton->link());
+
+        $form = $crawler->filter('form[name="login"]')->form();
+        $form['login[email]'] = 'newUser@email.com'; // Update with the desired email
+        $form['login[password]'] = 'password'; // Update with the desired password
+
+        return $client->submit($form);
     }
 
 
